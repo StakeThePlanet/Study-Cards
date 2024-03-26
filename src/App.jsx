fde71321dcddb9f9c6522a65cad49d8fecf7b6ca
FranciscoAndaur <francisco.andaur@icloud.com>
2024-03-26 10:29:00 -0400

    placed form and table side by side. need to fix form

--- src/App.jsx
+++ src/App.jsx
@@ -1,191 +1,196 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState } from "react";
+import { useEffect } from "react";
 // import React from 'react'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { supabase } from './assets/createClient'
-import Navbar  from './components/Navbar'
-import CreateFoodForm from './components/CreateFoodForm'
-import 'virtual:windi.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { supabase } from "./assets/createClient";
+import Navbar from "./components/Navbar";
+import CreateFoodForm from "./components/CreateFoodForm";
+import "virtual:windi.css";
 
 const App = () => {
-
-
   //Users state
-  const [users, setUsers] =  useState([])
+  const [users, setUsers] = useState([]);
   const [user, setUser] = useState({
-    name:'', job:'', company:''
-  })
-
-
+    name: "",
+    job: "",
+    company: "",
+  });
 
   //bev and food state
-  const [bevs, setBeverages] =  useState([])
-  const [food, setFood] =  useState([])
+  const [bevs, setBeverages] = useState([]);
+  const [food, setFood] = useState([]);
 
   useEffect(() => {
-    fetchUsers()
-    fetchbeverages()
-    fetchfood()
+    fetchUsers();
+    fetchbeverages();
+    fetchfood();
     // fetchCompanyNames()
-  }, [])
+  }, []);
 
   // function fetchCompanyNames(){
-   
+
   // }
-  
 
-///FETCHES
+  ///FETCHES
 
-//TODO remove the functionality to get company names outside of this function so it doesnt run every time it fetches an user.
+  //TODO remove the functionality to get company names outside of this function so it doesnt run every time it fetches an user.
 
-  async function fetchUsers(){
-
-    const {data} = await supabase
-      .from('users')
-      .select('*')
-      setUsers(data)
+  async function fetchUsers() {
+    const { data } = await supabase.from("users").select("*");
+    setUsers(data);
     // Use a Set to keep track of unique company names
-    const company_dropdown = document.getElementById('companyDropdown');
-    const job_dropdown =  document.getElementById('jobDropdown');
-  
-    const uniqueCompanyNames = [...new Set(data.map(item => item.company))];
-    console.log(uniqueCompanyNames)
-    const uniquejobs = [...new Set(data.map(item => item.job))];
-    console.log(uniquejobs)
-     uniqueCompanyNames.forEach(companyName => {
-      const option = document.createElement('option');
+    const company_dropdown = document.getElementById("companyDropdown");
+    const job_dropdown = document.getElementById("jobDropdown");
+
+    const uniqueCompanyNames = [...new Set(data.map((item) => item.company))];
+    console.log(uniqueCompanyNames);
+    const uniquejobs = [...new Set(data.map((item) => item.job))];
+    console.log(uniquejobs);
+    uniqueCompanyNames.forEach((companyName) => {
+      const option = document.createElement("option");
       option.value = companyName;
       option.text = companyName;
       company_dropdown.add(option);
     });
-    uniquejobs.forEach(jobName => {
-      const option = document.createElement('option');
+    uniquejobs.forEach((jobName) => {
+      const option = document.createElement("option");
       option.value = jobName;
       option.text = jobName;
       job_dropdown.add(option);
     });
     // Create options for each unique company name
-   
   }
-  async function fetchbeverages(){
-    const {data} = await supabase
-      .from('beverages')
-      .select('*')
-      setBeverages(data)
-    }
-  async function fetchfood(){
-    const {data} = await supabase
-      .from('food')
-      .select('*')
-      setFood(data)
-    }
+  async function fetchbeverages() {
+    const { data } = await supabase.from("beverages").select("*");
+    setBeverages(data);
+  }
+  async function fetchfood() {
+    const { data } = await supabase.from("food").select("*");
+    setFood(data);
+  }
 
-    
-  function handleChange(e){
-    setUser(prevFormData=>{
-      return{
+  function handleChange(e) {
+    setUser((prevFormData) => {
+      return {
         ...prevFormData,
-        [e.target.name]:e.target.value
-      }
-    })
+        [e.target.name]: e.target.value,
+      };
+    });
   }
 
-    // // Function to render FlipCard components based on data
-    // const renderFlipCards = (data) => {
-    //   return data.map((cardData, index) => (
-    //     <FlipCard key={index} frontContent={cardData.frontContent} backContent={cardData.backContent} />
-    //   ));
-    // };
+  // // Function to render FlipCard components based on data
+  // const renderFlipCards = (data) => {
+  //   return data.map((cardData, index) => (
+  //     <FlipCard key={index} frontContent={cardData.frontContent} backContent={cardData.backContent} />
+  //   ));
+  // };
 
   return (
     <Router>
-    <div>
-    <Navbar/>
-        <br/>
-    <CreateFoodForm />
+      <div>
+        <Navbar />
+        <br />
+        <div className="grid grid-cols-2 gap-6">
+  <form className="bg-white rounded-lg shadow-lg p-6">
+  <CreateFoodForm />
+  </form>
 
+  <div>
     
+    <h1>FOOD:</h1>
+    <table className="border-collapse border border-gray-300">
+  <thead>
+    <tr>
+      <th className="border border-gray-300 px-4 py-2">ID</th>
+      <th className="border border-gray-300 px-4 py-2">Name</th>
+      <th className="border border-gray-300 px-4 py-2">Drop Line</th>
+      <th className="border border-gray-300 px-4 py-2">Allergy</th>
+      <th className="border border-gray-300 px-4 py-2">Mies en Place</th>
+      <th className="border border-gray-300 px-4 py-2">Type</th>
+    </tr>
+  </thead>
+  <tbody>
+    {food.map((food) => (
+    <tr key={food.food_id} className="border border-gray-300">
+      <td className="border border-gray-300 px-4 py-2">{food.food_id}</td>
+      <td className="border border-gray-300 px-4 py-2">{food.food_name}</td>
+      <td className="border border-gray-300 px-4 py-2">{food.drop_line}</td>
+      <td className="border border-gray-300 px-4 py-2">{food.allergy}</td>
+      <td className="border border-gray-300 px-4 py-2">{food.mies_en_place}</td>
+      <td className="border border-gray-300 px-4 py-2">{food.type}</td>
+    </tr>
+    ))}
+  </tbody>
+</table>
+    
+  </div>
+</div>
 
-      <br/>    <br/>    <br/>
+        
+        <br /> <br /> <br />
+        <p>🍽️ Are you ready for a culinary adventure? Introducing our upcoming app designed exclusively for restaurant owners and managers.
 
-    <h1>USERS:</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Job</th>
-            <th>Company</th>
-            <th>Company ID</th>
-          </tr>
-        </thead>
-        <tbody>
-        {users.map((user) =>
-          <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.job}</td>
-              <td>{user.company}</td>
-              <td>{user.company_id}</td>
-          </tr>
-        )} 
-        </tbody>
-      </table>
-      <br/>    <br/>    <br/>
-      <h1>FOOD:</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>drop_line</th>
-            <th>allergy</th>
-            <th>mies_en_place</th>
-            <th>type</th>
-          </tr>
-        </thead>
-        <tbody>
-        {food.map((food) =>
-          <tr key={food.food_id}>
-              <td>{food.food_id}</td>
-              <td>{food.name}</td>
-              <td>{food.drop_line}</td>
-              <td>{food.allergy}</td>
-              <td>{food.mies_en_place}</td>
-              <td>{food.type}</td>
-          </tr>
-        )} 
-        </tbody>
-      </table>
-      <br/>    <br/>    <br/>
-      <h1>BEVERAGES:</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>spirit</th>
-            <th>ingredients</th>
-            <th>drop_line</th>
-            <th>allergy</th>
-          </tr>
-        </thead>
-        <tbody>
-        {bevs.map((bevs) =>
-          <tr key={bevs.id}>
-              <td>{bevs.id}</td>
-              <td>{bevs.name}</td>
-              <td>{bevs.main_Spirit}</td>
-              <td>{bevs.ingredients}</td>
-              <td>{bevs.drop_line}</td>
-              <td>{bevs.allergy}</td>
-          </tr>
-        )} 
-        </tbody>
-      </table>
-    </div>
+🚧 Currently under construction, this revolutionary platform is tailored to keep your staff informed about the latest additions to your menu, ensuring everyone stays up-to-date with your restaurant's gastronomic journey.
+
+🍲 Stay tuned as we craft an intuitive experience that empowers your team to explore and embrace new food items effortlessly. From mouth-watering entrees to delectable desserts, our app will be your go-to destination for culinary innovation.
+
+🔧 We're hard at work behind the scenes to bring you a seamless solution that enhances communication and excitement within your restaurant's kitchen. Get ready to elevate your dining experience like never before!
+
+Keep an eye out for updates as we prepare to launch this game-changing app. Your restaurant's culinary revolution starts here! 🚀👨‍🍳👩‍🍳</p>
+        <h1>USERS:</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Company</th>
+              <th>Company ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.job}</td>
+                <td>{user.company}</td>
+                <td>{user.company_id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <br /> <br /> <br />
+        
+        <br /> <br /> <br />
+        <h1>BEVERAGES:</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>spirit</th>
+              <th>ingredients</th>
+              <th>drop_line</th>
+              <th>allergy</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bevs.map((bevs) => (
+              <tr key={bevs.id}>
+                <td>{bevs.id}</td>
+                <td>{bevs.name}</td>
+                <td>{bevs.main_Spirit}</td>
+                <td>{bevs.ingredients}</td>
+                <td>{bevs.drop_line}</td>
+                <td>{bevs.allergy}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/CreateFoodForm.jsx
+++ src/components/CreateFoodForm.jsx
@@ -1,39 +1,37 @@
-import  { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import 'virtual:windi.css'
-import { supabase } from '../assets/createClient';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "virtual:windi.css";
+import { supabase } from "../assets/createClient";
 // import { Link } from 'react-router-dom';
 // import Modal from './modal/modal';
 
 const CreateFoodForm = () => {
   //food create form state
   const [formData, setFormData] = useState({
-    food_name: '',
-    drop_line: '',
-    prep_methods: '',
-    allergy: '',
-    mies_en_place: '',
-    bev_pairing: '',
-    type: ''
+    food_name: "",
+    drop_line: "",
+    prep_methods: "",
+    allergy: "",
+    mies_en_place: "",
+    bev_pairing: "",
+    type: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       // Insert new food item into the 'food' table
-      const { data, error } = await supabase
-        .from('food')
-        .insert([formData]);
+      const { data, error } = await supabase.from("food").insert([formData]);
 
       if (error) {
         throw error;
@@ -41,98 +39,198 @@ const CreateFoodForm = () => {
 
       // Reset form data after successful submission
       setFormData({
-        food_name: '',
-        drop_line: '',
-        prep_methods: '',
-        allergy: '',
-        mies_en_place: '',
-        bev_pairing: '',
-        type: ''
+        food_name: "",
+        drop_line: "",
+        prep_methods: "",
+        allergy: "",
+        mies_en_place: "",
+        bev_pairing: "",
+        type: "",
       });
 
-      alert('New food item created successfully!');
+      alert(`New food item created successfully!`);
     } catch (error) {
-      console.error('Error creating food item:', error.message);
-      alert('Failed to create new food item.');
+      console.error("Error creating food item:", error.message);
+      alert("Failed to create new food item.");
     }
   };
-    return (
-        <>
-        <h1 className="text-4xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text">Test form to Create Food:</h1>
-  <form onSubmit={handleSubmit}>
-   
-   <div>
-   <label>food name: </label>
-   <input
-     type="text"
-     placeholder="food name"
-     className="food_name"
-     name="food_name"
-     value={formData.food_name}
-     onChange={handleChange}
-   /><br />
-   <label>drop line: </label>
-   <input
-     type="text"
-     placeholder="drop line"
-     className="drop_line"
-     name="drop_line"
-     value={formData.drop_line}
-     onChange={handleChange}
-   /><br />
-   <label>Prep Methods: </label>
-   <input
-     type="text"
-     placeholder="prep methods"
-     className="prep_methods"
-     name="prep_methods"
-     value={formData.prep_methods}
-     onChange={handleChange}
-   /><br />
-   <label>allergies: </label>
-   <input
-     type="text"
-     placeholder="allergies"
-     className="allergy"
-     name="allergy"
-     value={formData.allergy}
-     onChange={handleChange}
-   /><br />
-   <label>Mies: </label>
-   <input
-     type="text"
-     placeholder="mies en place"
-     className="mies_en_place"
-     name="mies_en_place"
-     value={formData.mies_en_place}
-     onChange={handleChange}
-   /><br />
-   <label>Beverage Pairing*: </label>
-   <input
-     type="text"
-     placeholder="brev pairing"
-     className="bev_pairing"
-     name="bev_pairing"
-     value={formData.bev_pairing}
-     onChange={handleChange}
-   /><br />
-   <label>Course<span className='italic'>(Cold App, Warm App, Entree, Dessert): </span></label>
-   <input
-     type="text"
-     placeholder="course"
-     className="type"
-     name="type"
-     value={formData.type}
-     onChange={handleChange}
-   /><br />
- </div>
- <button type="submit" className="bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text hover:bg-gradient-to-l hover:from-pink-500 hover:to-purple-500 hover:text-white py-2 px-4 rounded-md border border-transparent shadow-md hover:shadow-lg">
-        Submit
-    </button>
-    </form>
-        </>
-      );
-    };
-    
-    export default CreateFoodForm
-    
+  return (
+    <>
+      <h1 className="text-4xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text">
+        Test form to Create Food:
+      </h1>
+      <div className="flex items-center justify-center h-screen">
+  <form className="border border-gray-300 p-6 rounded-md max-w-md" onSubmit={handleSubmit}>
+    <div className="flex flex-col space-y-4">
+    <label>food name: </label>
+          <input
+            type="text"
+            placeholder="food name"
+            className="food_name type p-2 border border-gray-300 rounded-md"
+            name="food_name"
+            value={formData.food_name}
+            onChange={handleChange}
+          />
+          <label>drop line: </label>
+          <input
+            type="text"
+            placeholder="drop line"
+            className="drop_line p-2 border border-gray-300 rounded-md"
+            name="drop_line"
+            value={formData.drop_line}
+            onChange={handleChange}
+          />
+          
+          <label>Prep Methods: </label>
+          <input
+            type="text"
+            placeholder="prep methods"
+            className="prep_methods p-2 border border-gray-300 rounded-md"
+            name="prep_methods"
+            value={formData.prep_methods}
+            onChange={handleChange}
+          />
+          <label>allergies: </label>
+          <input
+            type="text"
+            placeholder="allergies"
+            className="allergy p-2 border border-gray-300 rounded-md"
+            name="allergy"
+            value={formData.allergy}
+            onChange={handleChange}
+          />
+          <label>Mies: </label>
+          <input
+            type="text"
+            placeholder="mies en place"
+            className="mies_en_place p-2 border border-gray-300 rounded-md"
+            name="mies_en_place"
+            value={formData.mies_en_place}
+            onChange={handleChange}
+          />
+          <label>Beverage Pairing*: </label>
+          <input
+            type="text"
+            placeholder="brev pairing"
+            className="bev_pairing p-2 border border-gray-300 rounded-md"
+            name="bev_pairing"
+            value={formData.bev_pairing}
+            onChange={handleChange}
+          />
+          <label className='text-center'>
+            Course
+            <span className="italic">
+              (Cold App, Warm App, Entree, Dessert):{" "}
+            </span>
+          </label>
+          <input
+            type="text"
+            placeholder="course"
+            className="type p-2 border border-gray-300 rounded-md"
+            name="type"
+            value={formData.type}
+            onChange={handleChange}
+          />
+          <div className="flex justify-end">
+          <button
+          type="submit"
+
+          className="bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text hover:bg-gradient-to-l hover:from-pink-500 hover:to-purple-500 hover:text-white py-2 px-4 rounded-md border border-transparent shadow-md hover:shadow-lg"
+        >
+          Submit
+        </button>
+          </div>
+          
+        
+    </div>
+
+  </form>
+    </div>
+
+{/* 
+      <form >
+        <div className="flex flex-col items-center">
+        
+        <label>food name: </label>
+          <input
+            type="text"
+            placeholder="food name"
+            className="food_name"
+            name="food_name"
+            value={formData.food_name}
+            onChange={handleChange}
+          />
+          <label>drop line: </label>
+          <input
+            type="text"
+            placeholder="drop line"
+            className="drop_line"
+            name="drop_line"
+            value={formData.drop_line}
+            onChange={handleChange}
+          />
+          
+          <label>Prep Methods: </label>
+          <input
+            type="text"
+            placeholder="prep methods"
+            className="prep_methods"
+            name="prep_methods"
+            value={formData.prep_methods}
+            onChange={handleChange}
+          />
+          <label>allergies: </label>
+          <input
+            type="text"
+            placeholder="allergies"
+            className="allergy"
+            name="allergy"
+            value={formData.allergy}
+            onChange={handleChange}
+          />
+          <label>Mies: </label>
+          <input
+            type="text"
+            placeholder="mies en place"
+            className="mies_en_place"
+            name="mies_en_place"
+            value={formData.mies_en_place}
+            onChange={handleChange}
+          />
+          <label>Beverage Pairing*: </label>
+          <input
+            type="text"
+            placeholder="brev pairing"
+            className="bev_pairing"
+            name="bev_pairing"
+            value={formData.bev_pairing}
+            onChange={handleChange}
+          />
+          <label>
+            Course
+            <span className="italic">
+              (Cold App, Warm App, Entree, Dessert):{" "}
+            </span>
+          </label>
+          <input
+            type="text"
+            placeholder="course"
+            className="type"
+            name="type"
+            value={formData.type}
+            onChange={handleChange}
+          />
+          <br />
+          <button
+          type="submit"
+          className="bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text hover:bg-gradient-to-l hover:from-pink-500 hover:to-purple-500 hover:text-white py-2 px-4 rounded-md border border-transparent shadow-md hover:shadow-lg"
+        >
+          Submit
+        </button>
+        </div>
+      </form> */}
+    </>
+  );
+};
+
+export default CreateFoodForm;
